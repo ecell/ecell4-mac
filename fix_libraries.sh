6227b08363405325ad6dc0e9b4c50dbbe331115b
Suguru Kato <salmon.bioinformatics@gmail.com>
2019-02-14 17:28:50 +0900

    :construction: Testing fix_libraries.sh with CI

--- fix_libraries.sh
+++ fix_libraries.sh
@@ -1,25 +1,28 @@
 #!/usr/bin/env bash
 
-cd ecell4-base/python/dist
-
+readonly package=ecell4_base
 readonly pattern='\(libicu[a-z0-9]*\.[0-9]*\)\.\([0-9]*\)\.dylib'
 
-readonly wheel=$(ls *.whl)
-unzip $wheel
+readonly cwd=$(pwd)
+readonly wheel=${cwd}/$(ls ecell4-base/python/dist/*.whl)
+readonly tmpdir=$(mktemp -d)
+
+cd ${tmpdir}
+unzip ${wheel}
 
 echo "Fixing ..."
-for dylib in $(ls ecell4/.dylibs/libicu*.dylib); do
+for dylib in $(ls ${package}/.dylibs/libicu*.dylib); do
     change_option=$(basename "$dylib" | grep -e "$pattern" | sed -e "s:^${pattern}$:@loader_path/\1.dylib @loader_path/\1.\2.dylib:g")
     echo "change_option = $change_option"
-    for target in $(ls ecell4/.dylibs/lib*.dylib); do
-        test -w "$target" && echo "install_name_tool -change $change_option $target" && install_name_tool -change $change_option $target
+    for target in $(ls ${package}/.dylibs/lib*.dylib); do
+        test -w "${target}" && echo "install_name_tool -change $change_option ${target}" && install_name_tool -change $change_option ${target}
     done
 done
 
 echo "Checking ..."
-for dylib in $(ls ecell4/.dylibs/lib*.dylib); do
-  otool -L $dylib
+for dylib in $(ls ${package}/.dylibs/lib*.dylib); do
+  otool -L ${dylib}
 done
 
-rm -rf $wheel
-zip -r $wheel $(ls -d ecell*)
+zip -r $wheel *
+cd ${cwd}
